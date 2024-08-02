2b19d8d56a2902bd1fbbba22f542fbc7a5120cec
Ruben Villalvazo <vim.ruben@hotmail.com>
2024-08-01 23:46:46 -0300

    Refact: Better use of types and recursion

--- src/main.rs
+++ src/main.rs
@@ -2,7 +2,20 @@ use rand::Rng;
 use std::cmp::Ordering;
 use std::io;
 
-fn user_input() -> String {
+enum InputType {
+    Guess,
+    PlayAgain,
+}
+
+fn user_input(input_type: InputType) -> String {
+    match input_type {
+        InputType::Guess => {
+            println!("Input your guess: ");
+        }
+        InputType::PlayAgain => {
+            println!("Would you like to play again? (Y/N)");
+        }
+    }
     let mut user_input: String = String::new();
     io::stdin()
         .read_line(&mut user_input)
@@ -11,45 +24,43 @@ fn user_input() -> String {
 }
 
 fn game() {
-    println!("GUESS THE NUMBER!");
+    loop {
+        println!("GUESS THE NUMBER!");
 
-    let secret_number = rand::thread_rng().gen_range(1..=100);
+        let secret_number = rand::thread_rng().gen_range(1..=100);
 
-    'game_loop: loop {
-        println!("Input your guess: ");
-        let user_guess: i32 = match user_input().trim().parse() {
-            Ok(num) => num,
-            Err(_) => {
-                println!("Please type a number!");
+        loop {
+            let user_guess: i32 = match user_input(InputType::Guess).trim().parse() {
+                Ok(num) => num,
+                Err(_) => {
+                    println!("Please type a number!");
+                    continue;
+                }
+            };
+
+            if user_guess < 1 || user_guess > 100 {
+                println!("The secret number will be between 1 and 100.");
                 continue;
             }
-        };
 
-        if user_guess < 1 || user_guess > 100 {
-            println!("The secret number will be between 1 and 100.");
-            continue;
+            match user_guess.cmp(&secret_number) {
+                Ordering::Less => println!("Too small!"),
+                Ordering::Greater => println!("Too big!"),
+                Ordering::Equal => {
+                    println!("You win!");
+                    break;
+                }
+            }
         }
 
-        match user_guess.cmp(&secret_number) {
-            Ordering::Less => println!("To small!"),
-            Ordering::Greater => println!("To big!"),
-            Ordering::Equal => {
-                println!("You win!");
-                println!("Would you like to play again? (Y/N)");
-
-                loop {
-                    let user_answer = user_input().trim().to_lowercase();
-                    if user_answer == "n" || user_answer == "no" {
-                        break 'game_loop;
-                    } else if user_answer == "y" || user_answer == "yes" {
-                        game();
-                        break 'game_loop;
-                    } else {
-                        println!("Please enter a valid answer.");
-                        println!("Would you like to play again? (Y/N)");
-                        continue;
-                    }
-                }
+        loop {
+            let user_answer = user_input(InputType::PlayAgain).trim().to_lowercase();
+            if user_answer == "n" || user_answer == "no" {
+                return;
+            } else if user_answer == "y" || user_answer == "yes" {
+                break;
+            } else {
+                println!("Please enter a valid answer.");
             }
         }
     }
